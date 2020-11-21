225c981b4b9a07b35eb5a776bd5b99629e78bd40
nlopez <nelly.lopez@utexas.edu>
2020-11-21 13:28:54 -0600

    initial files

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,34 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your username?',
+    }
+    { 
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    return fs.writeToFileSync(path.join(process.cddw(),filename), data);
+    }
+
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+    }
 
 }
 
 // function call to initialize program
 init();
+
+
